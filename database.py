f8be756884840227351e04c101f8ee638031a35a
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2020-05-22 13:55:51 +0400

    Add files via upload

--- database.py
+++ database.py
@@ -17,15 +17,15 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 def main():
-    mycursor.execute('create table music (sno int,singer varchar(20),decade varchar(3),genre varchar(20),language varchar(20),link varchar(100))')
+    mycursor.execute('create table music (sno int,singer varchar(100),decade varchar(3),genre varchar(20),language varchar(20),link varchar(100))')
     mydb.commit()
-    music = pd.read_csv('/Users/Sathish/Desktop/project/musicdb.csv')
+    music = pd.read_csv('./musicdb.csv')
     for i in range(len(music)):
         mycursor.execute('insert into music values(\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\")'.format(i+1,(music['singer'])[i],(music['decade'])[i],(music['genre'])[i],(music['language'])[i],(music['ytlink'])[i]))
         mydb.commit()
-    mycursor.execute('create table movies (sno int,title varchar(20),genre varchar(10),language varchar(20),decade varchar(20),link varchar(100))')
+    mycursor.execute('create table movies (sno int,title varchar(100),genre varchar(10),language varchar(20),decade varchar(20),link varchar(100))')
     mydb.commit()
-    movie = pd.read_csv('/Users/Sathish/Desktop/project/moviedb.csv')
+    movie = pd.read_csv('./moviedb.csv')
     for i in range(len(movie)):
         mycursor.execute('insert into movies values(\"{}\",\"{}\",\"{}\",\"{}\",\"{}\",\"{}\")'.format(i+1,(movie['title'])[i],(movie['genre'])[i],(movie['language'])[i],(movie['decade'])[i],(movie['link'])[i]))
         mydb.commit()
